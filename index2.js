4fe73e8bbec151ec532266b4bdb46f102f1d94b6
ihda06 <ihdaanwari5@gmail.com>
2022-09-28 17:44:02 +0700

    auto start when web opened

--- index2.js
+++ index2.js
@@ -1,11 +1,7 @@
-// require('dotenv').config();
-// const botMenfess = require('./index')
 import menfessBot, { getDM } from "./index.js";
 import Express from "express";
 import Cron from "node-cron";
-import open from "open";
 
-// const express = require('express');
 const bot = menfessBot;
 
 const app = Express();
@@ -20,31 +16,15 @@ const task = Cron.schedule(
   },
   { scheduled: false }
 );
-const wakeUp = Cron.schedule(
-  "*/29 0-23 * * *",
-  async () => {
-    try {
-      await open("https://bot-menfess-astra.herokuapp.com/on");
-      console.log("Bot restarted");
-    } catch (error) {
-      console.log(error);
-    }
-  },
-  { scheduled: false }
-);
 
-// const { Twitterbot } = require('./twitter-bot');
-// const twitterBot = new Twitterbot({
-//     appKey: process.env.APP_KEY,
-//     appSecret: process.env.APP_SECRET,
-//     accessToken: process.env.ACCESS_TOKEN,
-//     accessSecret: process.env.ACCESS_SECRET,
-//     clientId: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-// })
-
-app.get("/", (req, res) => {
-  res.send("welcome to pembelajaran bot twitter");
+app.get("/", async (req, res) => {
+  res.write("<h1>welcome to pembelajaran bot twitter</h1>");
+  res.write("<h4>Initializing bot<h4>");
+  res.end();
+  task.start();
+  res.write("<h4>Task started<h4>");
+  res.end();
+  console.log("Task started");
 });
 
 app.get("/getadmin", async (req, res, next) => {
@@ -69,16 +49,6 @@ app.get("/stop", async (req, res, next) => {
   task.stop();
   res.send("job stopped");
   console.log("job stopped");
-});
-
-app.get("/cekopen", async (req, res, next) => {
-  res.send("congrats brow")
-  console.log("opened");  
-});
-
-app.get("/open", async (req,res, next) => {
-  open('https://bot-menfess-astra.herokuapp.com/cekopen')
-  res.send("wake up")
 });
 
 app.listen(port, () => {
